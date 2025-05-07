9fbbef5aaa9e26c1b2aad3ad2323ddaf684b4580
Dagtret <sergeyaggo2015@gmail.com>
2025-05-07 14:14:42 +0300

    Поправил замечание

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -95,7 +95,7 @@ class APIClient:
     def get_booking_ids(self, params=None):
         with allure.step('Get booking ids'):
             url = f'{self.base_url}{Endpoints.BOOKING_ENDPOINT}'
-            response = self.session.post(url, params=params)
+            response = self.session.get(url, params=params)
             response.raise_for_status()
         with allure.step('Checking status code'):
             assert response.status_code == 200, f'Expected status 200 but got {response.status_code}'
