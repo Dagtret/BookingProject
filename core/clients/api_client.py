544e9cd3d35d4686f2d0e062b3b0b31d4a6e5b64
Dagtret <sergeyaggo2015@gmail.com>
2025-05-14 10:36:46 +0300

    Написал один тест для проверки создания отелей

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -86,10 +86,13 @@ class APIClient:
     def create_booking(self, booking_data):
         with allure.step('Create booking'):
             url = f'{self.base_url}{Endpoints.BOOKING_ENDPOINT.value}'
-            response = self.session.post(url, json=booking_data)
+            headers = {
+                'Accept': 'application/json'
+            }
+            response = self.session.post(url,headers=headers, json=booking_data)
             response.raise_for_status()
         with allure.step('Checking status code'):
-            assert  response.status_code == 201, f'Expected status 201 but got {response.status_code}'
+            assert  response.status_code == 200, f'Expected status 200 but got {response.status_code}'
         return response.json()
 
     def get_booking_ids(self, params=None):
--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -5,5 +5,5 @@ import requests
 @allure.feature('Test booking')
 @allure.story('Test create booking')
 def test_create_booking(api_client, generate_random_booking_data):
-    status_code = api_client.create_booking(generate_random_booking_data)
-    assert status_code == 201, f'Expected status 201 but got {status_code}'
+    response = api_client.create_booking(booking_data=generate_random_booking_data)
+
